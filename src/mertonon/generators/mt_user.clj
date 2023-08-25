3bfe74306cb305906b8725c0e214517e7c7cd028
Howon Lee <hlee.howon@gmail.com>
2023-08-25 11:27:38 -0700

    thinky

--- src/mertonon/generators/mt_user.clj
+++ src/mertonon/generators/mt_user.clj
@@ -1,5 +1,5 @@
 (ns mertonon.generators.mt-users
-  "Generating mertonon users"
+  "Generating mertonon users and authentication methods"
   (:require [clojure.test.check :as tc]
             [clojure.test.check.clojure-test :refer :all]
             [clojure.test.check.generators :as gen]
@@ -10,12 +10,13 @@
             [loom.alg-generic :as graph-alg-generic]))
 
 (defn generate-mt-user*
-  [{:keys [name-type label-type] :as params}]
-  (gen/let [mt-user-uuid  gen/uuid
-            mt-user-name  (gen-data/gen-user-names name-type)
-            mt-user-label (gen-data/gen-labels label-type)]
-    [(mtc/->MtUser mt-user-uuid
-                   mt-user-name
-                   mt-user-label)]))
+  [{:keys [name-type] :as params}]
+  (gen/let [mt-user-uuid     gen/uuid
+            mt-user-email    (gen/data-gen-mt-user-emails name-type)
+            mt-user-username (gen-data/gen-mt-user-usernames name-type)]
+    {:mt-users [(mtc/->MtUser mt-user-uuid
+                              mt-user-email
+                              mt-user-username)]}))
 
-
+;; (def generate-mt-user
+;;   some crap)
