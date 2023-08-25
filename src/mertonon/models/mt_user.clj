b059aee8a5abed35e8eb5ab2a2adfcca6395aaa0
Howon Lee <hlee.howon@gmail.com>
2023-08-25 11:25:42 -0700

    comment out that bit

--- src/mertonon/models/mt_user.clj
+++ src/mertonon/models/mt_user.clj
@@ -14,32 +14,32 @@
 ;;;;;;;;
 ;;;;;;;;
 
-(defn canonicalize-grid [grid]
-  (-> (mutils/default-canonicalize grid)
-      (update :optimizer-type keyword)
-      (update :version io/maybe-parse-int)))
-
-(defn member->row [member]
-  (-> (canonicalize-grid member)
-      (mutils/default-member->row)
-      (update :hyperparams io/maybe-json-encode)))
-
-(defn row->member [row]
-  (-> (canonicalize-grid row)
-      (mutils/default-row->member)
-      (update :hyperparams io/maybe-json-decode)))
-
-(def columns [:uuid :version :created-at :updated-at :name :label :optimizer-type :hyperparams])
-(def table [:mertonon.grid])
-(def query-info {:columns     columns
-                 :table       table
-                 :member->row member->row
-                 :row->member row->member})
-
-(def model
-  (q/default-model query-info))
-
-(comment (let [member (-> (gen/generate net-gen/generate-grid)
-                          :grids
-                          first)]
-           (row->member (member->row member))))
+;; (defn canonicalize-grid [grid]
+;;   (-> (mutils/default-canonicalize grid)
+;;       (update :optimizer-type keyword)
+;;       (update :version io/maybe-parse-int)))
+;; 
+;; (defn member->row [member]
+;;   (-> (canonicalize-grid member)
+;;       (mutils/default-member->row)
+;;       (update :hyperparams io/maybe-json-encode)))
+;; 
+;; (defn row->member [row]
+;;   (-> (canonicalize-grid row)
+;;       (mutils/default-row->member)
+;;       (update :hyperparams io/maybe-json-decode)))
+;; 
+;; (def columns [:uuid :version :created-at :updated-at :name :label :optimizer-type :hyperparams])
+;; (def table [:mertonon.grid])
+;; (def query-info {:columns     columns
+;;                  :table       table
+;;                  :member->row member->row
+;;                  :row->member row->member})
+;; 
+;; (def model
+;;   (q/default-model query-info))
+;; 
+;; (comment (let [member (-> (gen/generate net-gen/generate-grid)
+;;                           :grids
+;;                           first)]
+;;            (row->member (member->row member))))
