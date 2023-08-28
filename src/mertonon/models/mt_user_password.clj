b7cc89638d2e9fa7c75954e1d25b830ca8f91474
Howon Lee <hlee.howon@gmail.com>
2023-08-28 14:39:37 -0700

    digestification, basically

--- src/mertonon/models/mt_user_password.clj
+++ src/mertonon/models/mt_user_password.clj
@@ -1,25 +1,36 @@
 (ns mertonon.models.mt-user-password
   "A password authentication for a user of mertonon"
   (:require [clojure.test.check.generators :as gen]
+            [crypto.password.scrypt :as scrypt]
             [mertonon.generators.mt-user :as mt-user-gen]
             [mertonon.models.utils :as mutils]
             [mertonon.util.io :as io]
             [mertonon.util.queries :as q]
             [tick.core :as t]))
 
+(defn digestify-password
+  "Idempotent"
+  [{:keys [password] :as mt-user-password}]
+  (if (nil? password)
+    mt-user-password
+    (-> mt-user-password
+        (assoc :password-digest (scrypt/encrypt password))
+        (dissoc :password))))
+
 (defn canonicalize-mt-user-password [mt-user-password]
   (-> (mutils/default-canonicalize mt-user-password)
-      ;; some crap
-      ))
+      (digestify-password)))
 
 (defn member->row [member]
-  (-> (canonicalize-mt-user member)
+  (-> (canonicalize-mt-user-password member)
       (mutils/default-member->row)))
 
 (defn row->member [row]
-  (-> (canonicalize-mt-user row)
+  (-> (canonicalize-mt-user-password row)
       (mutils/default-row->member)))
 
+;;;;
+;;;;
 ;;;;
 (def columns [:uuid :version :created-at :updated-at :email :username :canonical-username])
 (def table [:mertonon.mt_user_password])
@@ -31,4 +42,6 @@
 (def model
   (q/default-model query-info))
 
-(comment)
+(comment
+  (require '[crypto.password.scrypt :as scrypt])
+  (scrypt/encrypt "bleh"))
