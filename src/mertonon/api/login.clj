1881ac4e5d0d67d6c86eda4361fd1a6f8539dac6
Howon Lee <hlee.howon@gmail.com>
2023-08-30 11:53:20 -0700

    thinking

--- src/mertonon/api/login.clj
+++ src/mertonon/api/login.clj
@@ -1,12 +1,15 @@
 (ns mertonon.api.login
   "API for logging in. Not to be confused with password-login"
   (:require [clojure.data.json :as json]
+            [clojure.walk :as walk]
             [mertonon.api.util :as api-util]
+            [mertonon.util.io :as uio]
             [mertonon.util.uuid :as uutils]))
 
 (defn do-login [m]
-  (println (:body-params m))
-  nil)
+  (let [body   (-> m :body-params uio/maybe-slurp uio/maybe-json-decode walk/keywordize-keys)
+        printo (println body)]
+    nil))
 
 (defn login-endpoint []
   {:post do-login
