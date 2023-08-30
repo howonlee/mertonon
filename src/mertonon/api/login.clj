94ef4d6975188738a9c70fecc43b7756d16674ff
Howon Lee <hlee.howon@gmail.com>
2023-08-30 10:42:22 -0700

    add login endpoint

---
+++ src/mertonon/api/login.clj
@@ -0,0 +1,12 @@
+(ns mertonon.api.login
+  "API for logging in. Not to be confused with password-login"
+  (:require [clojure.data.json :as json]
+            [mertonon.api.util :as api-util]
+            [mertonon.util.uuid :as uutils]))
+
+(defn login-endpoint []
+  {:post do-login
+   :name ::login})
+
+(defn routes []
+  [["/" (login-endpoint)]])
