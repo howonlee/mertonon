04074eef5ddbe0d585f6b16e5ff194d2ec1d21c0
Howon Lee <hlee.howon@gmail.com>
2023-09-08 21:23:56 -0700

    split things off into setup

---
+++ src/mertonon/setup.clj
@@ -0,0 +1,34 @@
+(ns mertonon.setup
+  (:require [mertonon.api.generators :as generator-api]
+            [mertonon.models.constructors :as mc]
+            [mertonon.server.handler :as handler]
+            [mertonon.util.io :as uio]
+            [mertonon.util.migrations :as migrations]
+            [mertonon.util.uuid :as uutils]
+            [migratus.core :as migratus]
+            [mount.core :as mount :refer [defstate]]
+            [taoensso.timbre :as timbre :refer [log]]))
+
+(defn- db-setup! []
+  (do
+    (migratus/init migrations/config)
+    (migratus/migrate migrations/config)))
+
+(defstate db-migration
+  "DB migration"
+  :start (db-setup!))
+
+(defn- warmup!
+  []
+  (log :info "Warmup request sending...")
+  ((handler/app-handler) {:uri            "/api/v1/health_check"
+                          :request-method :post
+                          :body-params    (mc/->HealthCheck (uutils/uuid))})
+  (uio/load-array-impl!)
+  (log :info "Finished sending warmup request."))
+
+(defstate warmup
+  :start (warmup!))
+
+(defstate generate
+  :start (generator-api/generate-demo-net-atom-and-set!))
