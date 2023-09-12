b4c89fd560d6bde670cdaba01f4a6ad9b22c3f0d
Howon Lee <hlee.howon@gmail.com>
2023-09-12 12:19:56 -0700

    start on that middleware

---
+++ src/mertonon/server/middlewares/session.clj
@@ -0,0 +1,18 @@
+(ns mertonon.server.middlewares.session
+  "Want everything to be authn'ed except for the login endpoint itself, basically
+
+  This is allowlisting (whitelisting), which it really doesn't feel like. But it is."
+  (:require [ring.middleware.session :as ring-session]))
+
+(defn wrap-mertonon-session
+  "Synchronous only as of this time."
+  ([handler]
+   (wrap-mertonon-session handler {}))
+  ([handler options]
+   (let [options (#'ring-session/session-options options)]
+     (fn ([request]
+          ;;;; if login whitelist something
+          (let [request (ring-session/session-request request options)]
+            (-> (handler request)
+                (ring-session/session-response request options))))))))
+
