d168beaa26eff062d9b1fef49e0be0b6f2930626
Howon Lee <hlee.howon@gmail.com>
2023-09-12 11:27:01 -0700

    thinking

--- src/mertonon/server/handler.clj
+++ src/mertonon/server/handler.clj
@@ -12,28 +12,42 @@
             [reitit.ring.middleware.exception :as rr-exceptions]
             [taoensso.timbre :as timbre :refer [log]]))
 
-(defn base-router []
+;; ---
+;; Routing
+;; ---
+
+(defn- base-router-middlewares []
+  [params/wrap-params
+   muuntaja/format-middleware
+   ;; Note order matters.
+   ;; TODO: Tighten up origins
+   [wrap-cors :access-control-allow-origin [#".*"]
+    :access-control-allow-methods [:get :post]]
+   coercion/coerce-request-middleware
+   coercion/coerce-response-middleware])
+
+(defn- router [curr-routes curr-middlewares]
   (rr/router
-    [(routes/routes)]
+    [curr-routes]
     {:data {:muuntaja   m/instance
-            :middleware [params/wrap-params
-                         muuntaja/format-middleware
-                         ;; Note order matters.
-                         ;; TODO: Tighten up origins
-                         [wrap-cors :access-control-allow-origin [#".*"]
-                          :access-control-allow-methods [:get :post]]
-                         coercion/coerce-request-middleware
-                         coercion/coerce-response-middleware]}
+            :middleware curr-middlewares}
      :router r/trie-router}))
 
-(defn base-handler []
+(defn- base-router []
+  (router (routes/routes) (base-router-middlewares)))
+
+;; ---
+;; Handling
+;; ---
+
+(defn- base-handler []
   (rr/ring-handler
-    ;; TODO: dev / prod split, where prod is constantly router
-    ;; see https://github.com/metosin/reitit/blob/master/doc/advanced/dev_workflow.md
     (base-router)
     (rr/redirect-trailing-slash-handler)
     (rr/create-default-handler)))
 
 (defn app-handler [] (base-handler))
+
+(defn test-handler [] nil)
 
 (comment (base-router))
