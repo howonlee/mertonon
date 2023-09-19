13bee821736a5626c76d134f4e2809439f6cc461
Howon Lee <hlee.howon@gmail.com>
2023-09-19 10:58:12 -0700

    thinky

--- src/mertonon/api/intro.clj
+++ src/mertonon/api/intro.clj
@@ -3,32 +3,32 @@
   (:require [clojure.data.json :as json]
             [clojure.walk :as walk]
             [mertonon.api.util :as api-util]
+            [mertonon.models.constructors :as mtc]
+            [mertonon.models.mt-session :as mt-session-model]
             [mertonon.models.mt-user :as mt-user-model]
+            [mertonon.models.password-login :as password-login-model]
             [mertonon.server.middleware.validations :as val-mw]
             [mertonon.util.io :as uio]
             [mertonon.util.uuid :as uutils]
             [mertonon.util.validations :as uvals]
-            ))
+            [tick.core :as t]))
 
 (defn- do-intro [m]
   (let [body            (api-util/body-params m)
         ;; TODO: make this in a transaction for real
         username        (:username body)
-        printo          (println username)
-        ;;;;
-        ;;;;
-        ;;;;
-        mt-user!        nil
+        email           (:email body)
+        new-user        (mtc/->MtUser (uutils/uuid) email username)
+        mt-user!        ((mt-user-model/model :create-one!) new-user)
         password        (:password body)
-        ;; don't keep it
-        ;;;;
-        ;;;;
-        ;;;;
-        password-login! nil
-        ;;;;
-        ;;;;
-        ;;;;
-        session!        nil]
+        digest          (password-login-model/hash-password password)
+        new-password    (mtc/->PasswordLogin (uutils/uuid) (:uuid new-user) :default digest)
+        password-login! ((password-login-model/model :create-one!) new-password)
+        new-session     (mtc/->MtSession (uutils/uuid) (:uuid new-user)
+                                         ;; we don't actually have non-jank session exp now, so just say they're infinite
+                                         (t/>> (t/instant) (t/new-duration 100 :years))
+                                         new-user)
+        session!        ((mt-session-model/model :create-one!) new-session)]
     {:status 200
      :body {:mt-user mt-user!
             :session (:uuid session!)}}))
