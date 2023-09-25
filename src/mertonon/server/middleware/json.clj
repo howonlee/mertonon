417a4986ec8f4537e8b4d0f562fb482c2f9ad875
Howon Lee <hlee.howon@gmail.com>
2023-09-25 13:46:53 -0700

    how about we kill this one

--- src/mertonon/server/middleware/json.clj
+++ src/mertonon/server/middleware/json.clj
@@ -1,18 +1,14 @@
 (ns mertonon.server.middleware.json
   "ring-json doesn't work with data.json, cheshire only. which is why this exists"
   (:require [clojure.data.json :as json]
+            [clojure.string :as str]
             [ring.util.response :refer [content-type]]))
 
-(def json-endpoints
-  #{"/api/v1"})
+(def json-endpoints "/api/v1")
 
 (defn wrap-json
   [handler]
   (fn [request]
-    (if (contains? json-endpoints (:uri request))
-      (let [resp         (handler request)
-            updated-resp (update-in resp [:body] json/write-str)]
-        (if (contains? (:headers updated-resp) "Content-Type")
-          updated-resp
-          (content-type updated-resp "application/json; charset=utf-8")))
+    (if (some #(str/includes? % json-endpoints) (:uri request))
+      (update-in (handler request) [:body] json/write-str)
       (handler request))))
