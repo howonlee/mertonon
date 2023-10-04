ea3cfb746c320d3747cbc8cf0d147acf5c3025db
Howon Lee <hlee.howon@gmail.com>
2023-10-04 11:22:24 +0200

    add admin view basically

--- src/fe/mtfe/views/admin.cljs
+++ src/fe/mtfe/views/admin.cljs
@@ -3,6 +3,7 @@
   (:require [goog.string :as gstring]
             [mtfe.api :as api]
             [mtfe.stylecomps :as sc]
+            [mtfe.util :as util]
             [re-frame.core :refer [dispatch subscribe]]))
 
 (defn before-fx [m]
@@ -14,5 +15,25 @@
      [:<>
       [:h1 "Admin"]
       [:p "There are no non-admin accounts at this time. We will add normal user accounts when we do it."]
-      (for [mt-user mt-users] ^{:key (:uuid mt-user)}
-        [:pre (with-out-str (cljs.pprint/pprint mt-user))])])])
+      (when (seq mt-users)
+        [:<>
+         [:h2 "Users"]
+         [sc/main-table
+          [:thead
+           [:tr
+            [sc/table-head "UUID"]
+            [sc/table-head "Username"]
+            [sc/table-head "Email"]
+            [sc/table-head "Created Date"]
+            [sc/table-head "Updated Date"]
+            [sc/table-head ""]]]
+          [:tbody
+           (for [mt-user mt-users] ^{:key (:uuid mt-user)}
+             [:tr
+              [sc/table-member (str (:uuid mt-user))]
+              [sc/table-member (str (:username mt-user))]
+              [sc/table-member (str (:email mt-user))]
+              [sc/table-member (str (:created-at mt-user))]
+              [sc/table-member (str (:updated-at mt-user))]
+              [sc/table-member [util/sl (util/path ["mt_user" (:uuid mt-user) "delete"])
+                                [sc/trash-icon]]]])]]])])])
