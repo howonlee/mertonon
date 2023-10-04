2d296a2a0fdf9ae393294a548210208afa053c44
Howon Lee <hlee.howon@gmail.com>
2023-10-04 11:15:10 +0200

    gotta think about the table

--- src/fe/mtfe/views/admin.cljs
+++ src/fe/mtfe/views/admin.cljs
@@ -12,5 +12,7 @@
   [sc/main-section
    (let [mt-users @(subscribe [:selection :mt-users])]
      [:<>
+      [:h1 "Admin"]
+      [:p "There are no non-admin accounts at this time. We will add normal user accounts when we do it."]
       (for [mt-user mt-users] ^{:key (:uuid mt-user)}
         [:pre (with-out-str (cljs.pprint/pprint mt-user))])])])
