9e8a46d1500c5b42f6d9f5dd3a4f4f03d2da7d5d
Howon Lee <hlee.howon@gmail.com>
2023-10-05 21:01:16 +0200

    thinking about mutating state thing

--- src/fe/mtfe/components/form_inputs.cljs
+++ src/fe/mtfe/components/form_inputs.cljs
@@ -4,4 +4,51 @@
             [day8.re-frame.http-fx]
             [mtfe.stylecomps :as sc]
             [mtfe.util :as util]
-            [re-frame.core :refer [dispatch dispatch-sync reg-event-db reg-event-fx subscribe]]))
+            [reagent.core :as r]
+            [re-frame.core :refer [dispatch dispatch-sync reg-event-db reg-event-fx subscribe]]
+            ["react-datepicker" :default DatePicker]
+            ["react-tiny-popover" :refer [Popover]]))
+
+;; ---
+;; React class adapters
+;; ---
+
+(def date-picker (r/adapt-react-class DatePicker))
+(def popover (r/adapt-react-class Popover))
+
+;; ---
+;; Inputs and Form Stuff
+;; ---
+
+(defn state-text-input [state-path placeholder]
+  [sc/input {:type        "text"
+             :placeholder placeholder
+             :on-change   #(dispatch (into [:mutate-state] state-path))}])
+
+;; (defn state-password-input [state-path placeholder]
+;;   [sc/input {:type        "password"
+;;              :placeholder placeholder
+;;              :on-change   (mt-statechart/mutate-from-dom-event-handler state path)}])
+;; 
+;; (defn state-select-input [state-path choices path]
+;;   [sc/select {:on-change (mt-statechart/mutate-from-dom-event-handler sc-state path)
+;;               :value     (get-in @state path)}
+;;    [sc/select-option {:value ""} "---"]
+;;    (for [member choices] ^{:key (:uuid member)}
+;;      [sc/select-option {:value (:uuid member)} (:name member)])])
+;; 
+;; (defn state-range-input
+;;   ([state-path]
+;;   (state-range-input state-path 0 1000 5))
+;;   ([state-path _min _max step]
+;;    [sc/input {:type     "range"
+;;               :on-input (mt-statechart/mutate-from-dom-event-handler sc-state path)
+;;               :value    (get-in @state path)
+;;               :min      _min
+;;               :max      _max
+;;               :step     step}]))
+;; 
+;; (defn state-datepicker [state-path]
+;;   [date-picker {:popper-placement "left"
+;;                 :selected         (get-in @state path)
+;;                 :on-select        (mt-statechart/mutate-from-plain-function-handler sc-state path)}])
