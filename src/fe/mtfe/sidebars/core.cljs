797b426db96feae95b1d0bdf4c15c322a4482efc
Howon Lee <hlee.howon@gmail.com>
2023-10-12 10:48:44 -0700

    spacing

--- src/fe/mtfe/sidebars/core.cljs
+++ src/fe/mtfe/sidebars/core.cljs
@@ -14,6 +14,7 @@
             [mtfe.sidebars.layer :as layer]
             [mtfe.sidebars.loss :as loss]
             [mtfe.sidebars.mt-user :as mt-user]
+            [mtfe.sidebars.password-login :as password-login]
             [mtfe.sidebars.session :as session]
             [mtfe.sidebars.weight :as weight]
             [mtfe.sidebars.weightset :as weightset]
@@ -38,103 +39,141 @@
         that grid, or click the + button to create a new grid."]])
 
 (def sidebar-routes
-  [["/"                               {:name ::home-sidebar :view home-sidebar}]
-   ["/intro"                          {:name      ::intro-sidebar
-                                       :view      intro/intro-sidebar
-                                       :before-fx intro/intro-before-fx}]
-   ["/login"                          {:name      ::login-sidebar
-                                       :view      session/login-sidebar
-                                       :before-fx session/login-before-fx}]
-   ["/logout"                         {:name      ::logout-sidebar
-                                       :view      session/logout-sidebar
-                                       :before-fx session/logout-before-fx}]
-   ["/admin"                          {:name ::admin-sidebar :view admin/admin-sidebar}]
-   ["/admin/mt_user_create"           {:name      ::mt-user-create-sidebar
-                                       :view      mt-user/mt-user-create-sidebar
-                                       :before-fx mt-user/mt-user-create-before-fx}]
-   ["/mt_user"                        {:name ::mt-user-sidebar :view mt-user/mt-user-sidebar}]
-   ["/mt_user/:uuid/delete"           {:name      ::mt-user-delete-sidebar
-                                       :view      mt-user/mt-user-delete-sidebar
-                                       :before-fx mt-user/mt-user-delete-before-fx}]
+  [["/"
+    {:name ::home-sidebar :view home-sidebar}]
+   ["/intro"
+    {:name      ::intro-sidebar
+     :view      intro/intro-sidebar
+     :before-fx intro/intro-before-fx}]
+   ["/login"
+    {:name      ::login-sidebar
+     :view      session/login-sidebar
+     :before-fx session/login-before-fx}]
+   ["/logout"
+    {:name      ::logout-sidebar
+     :view      session/logout-sidebar
+     :before-fx session/logout-before-fx}]
+   ["/admin"
+    {:name ::admin-sidebar :view admin/admin-sidebar}]
+   ["/admin/mt_user_create"
+    {:name      ::mt-user-create-sidebar
+     :view      mt-user/mt-user-create-sidebar
+     :before-fx mt-user/mt-user-create-before-fx}]
+   ["/mt_user"
+    {:name ::mt-user-sidebar :view mt-user/mt-user-sidebar}]
+   ["/mt_user/:uuid/delete"
+    {:name      ::mt-user-delete-sidebar
+     :view      mt-user/mt-user-delete-sidebar
+     :before-fx mt-user/mt-user-delete-before-fx}]
+   ;; ["/mt_user/:uuid/password_login_create"
+   ;;  {:name      ::password-login-create-sidebar
+   ;;   :view      mt-user/mt-user-delete-sidebar
+   ;;   :before-fx mt-user/mt-user-delete-before-fx}]
 
-   ["/grid/:uuid/grad_kickoff"        {:name      ::grad-sidebar
-                                       :view      grad/grad-sidebar
-                                       :before-fx grad/grad-before-fx}]
+   ["/grid/:uuid/grad_kickoff"
+    {:name      ::grad-sidebar
+     :view      grad/grad-sidebar
+     :before-fx grad/grad-before-fx}]
 
-   ["/grid_create"                    {:name      ::grid-create-sidebar
-                                       :view      grid-select/grid-create-sidebar
-                                       :before-fx grid-select/grid-create-before-fx}]
-   ["/grid/:uuid/delete"              {:name      ::grid-delete-sidebar
-                                       :view      grid-select/grid-delete-sidebar
-                                       :before-fx grid-select/grid-delete-before-fx}]
-   ["/grid/:uuid"                     {:name      ::grid-sidebar
-                                       :view      grid/grid-sidebar
-                                       :before-fx grid/before-fx}]
-   ["/grid/:uuid/layer_create"        {:name      ::layer-create-sidebar
-                                       :view      layer/layer-create-sidebar
-                                       :before-fx layer/layer-create-before-fx}]
-   ["/grid/:uuid/weightset_create"    {:name      ::weightset-create-sidebar
-                                       :view      weightset/weightset-create-sidebar
-                                       :before-fx weightset/weightset-create-before-fx}]
-   ["/grid/:uuid/input_create"        {:name      ::input-create-sidebar
-                                       :view      input/input-create-sidebar
-                                       :before-fx input/input-create-before-fx}]
-   ["/grid/:uuid/loss_create"         {:name      ::loss-create-sidebar
-                                       :view      loss/loss-create-sidebar
-                                       :before-fx loss/loss-create-before-fx}]
-   ["/grid_demo"                      {:name      ::grid-demo-sidebar
-                                       :view      grid/grid-sidebar
-                                       :before-fx grid/demo-before-fx}]
+   ["/grid_create"
+    {:name      ::grid-create-sidebar
+     :view      grid-select/grid-create-sidebar
+     :before-fx grid-select/grid-create-before-fx}]
+   ["/grid/:uuid/delete"
+    {:name      ::grid-delete-sidebar
+     :view      grid-select/grid-delete-sidebar
+     :before-fx grid-select/grid-delete-before-fx}]
+   ["/grid/:uuid"
+    {:name      ::grid-sidebar
+     :view      grid/grid-sidebar
+     :before-fx grid/before-fx}]
+   ["/grid/:uuid/layer_create"
+    {:name      ::layer-create-sidebar
+     :view      layer/layer-create-sidebar
+     :before-fx layer/layer-create-before-fx}]
+   ["/grid/:uuid/weightset_create"
+    {:name      ::weightset-create-sidebar
+     :view      weightset/weightset-create-sidebar
+     :before-fx weightset/weightset-create-before-fx}]
+   ["/grid/:uuid/input_create"
+    {:name      ::input-create-sidebar
+     :view      input/input-create-sidebar
+     :before-fx input/input-create-before-fx}]
+   ["/grid/:uuid/loss_create"
+    {:name      ::loss-create-sidebar
+     :view      loss/loss-create-sidebar
+     :before-fx loss/loss-create-before-fx}]
+   ["/grid_demo"
+    {:name      ::grid-demo-sidebar
+     :view      grid/grid-sidebar
+     :before-fx grid/demo-before-fx}]
 
-   ["/layer/:uuid"                    {:name      ::layer-sidebar
-                                       :view      layer/layer-sidebar
-                                       :before-fx layer/layer-sidebar-before-fx}]
-   ["/layer_selection/:uuid"          {:name ::layer-selection-sidebar :view layer/layer-selection-sidebar}]
-   ["/layer/:uuid/delete"             {:name      ::layer-delete-sidebar
-                                       :view      layer/layer-delete-sidebar
-                                       :before-fx layer/layer-delete-before-fx}]
-   ["/layer/:uuid/cost_object_create" {:name      ::cost-object-create-sidebar
-                                       :view      cost-object/cost-object-create-sidebar
-                                       :before-fx cost-object/cost-object-create-before-fx}]
+   ["/layer/:uuid"
+    {:name      ::layer-sidebar
+     :view      layer/layer-sidebar
+     :before-fx layer/layer-sidebar-before-fx}]
+   ["/layer_selection/:uuid"
+    {:name ::layer-selection-sidebar :view layer/layer-selection-sidebar}]
+   ["/layer/:uuid/delete"
+    {:name      ::layer-delete-sidebar
+     :view      layer/layer-delete-sidebar
+     :before-fx layer/layer-delete-before-fx}]
+   ["/layer/:uuid/cost_object_create"
+    {:name      ::cost-object-create-sidebar
+     :view      cost-object/cost-object-create-sidebar
+     :before-fx cost-object/cost-object-create-before-fx}]
 
-   ["/cost_object/:uuid"              {:name      ::cost-object-sidebar
-                                       :view      cost-object/cost-object-sidebar
-                                       :before-fx cost-object/cost-object-sidebar-before-fx}]
-   ["/cost_object/:uuid/entry_create" {:name      ::entry-create-sidebar
-                                       :view      entry/entry-create-sidebar
-                                       :before-fx entry/entry-create-before-fx}]
-   ["/cost_object/:uuid/delete"       {:name      ::cost-object-delete-sidebar
-                                       :view      cost-object/cost-object-delete-sidebar
-                                       :before-fx cost-object/cost-object-delete-before-fx}]
-   ["/entry/:uuid/delete"             {:name      ::entry-delete-sidebar
-                                       :view      entry/entry-delete-sidebar
-                                       :before-fx entry/entry-delete-before-fx}]
+   ["/cost_object/:uuid"
+    {:name      ::cost-object-sidebar
+     :view      cost-object/cost-object-sidebar
+     :before-fx cost-object/cost-object-sidebar-before-fx}]
+   ["/cost_object/:uuid/entry_create"
+    {:name      ::entry-create-sidebar
+     :view      entry/entry-create-sidebar
+     :before-fx entry/entry-create-before-fx}]
+   ["/cost_object/:uuid/delete"
+    {:name      ::cost-object-delete-sidebar
+     :view      cost-object/cost-object-delete-sidebar
+     :before-fx cost-object/cost-object-delete-before-fx}]
+   ["/entry/:uuid/delete"
+    {:name      ::entry-delete-sidebar
+     :view      entry/entry-delete-sidebar
+     :before-fx entry/entry-delete-before-fx}]
+   ["/weightset/:uuid"
+    {:name      ::weightset-sidebar
+     :view      weightset/weightset-sidebar
+     :before-fx weightset/weightset-before-fx}]
 
-   ["/weightset/:uuid"                {:name      ::weightset-sidebar
-                                       :view      weightset/weightset-sidebar
-                                       :before-fx weightset/weightset-before-fx}]
-   ["/weightset_selection/:uuid"      {:name ::weightset-selection-sidebar :view weightset/weightset-selection-sidebar}]
-   ["/weightset/:uuid/delete"         {:name      ::weightset-delete-sidebar
-                                       :view      weightset/weightset-delete-sidebar
-                                       :before-fx weightset/weightset-delete-before-fx}]
-   ["/weightset/:uuid/weight_create"  {:name      ::weight-create-sidebar
-                                       :view      weight/weight-create-sidebar
-                                       :before-fx weight/weight-create-before-fx}]
-
-   ["/weight/:uuid"                   {:name ::weight-sidebar :view weight/weight-sidebar}]
-   ["/weight_selection/:uuid"         {:name      ::weight-selection-sidebar
-                                       :view      weight/weight-selection-sidebar
-                                       :before-fx weight/weight-selection-before-fx}]
-   ["/weight/:uuid/delete"            {:name      ::weight-delete-sidebar
-                                       :view      weight/weight-delete-sidebar
-                                       :before-fx weight/weight-delete-before-fx}]
+   ["/weightset_selection/:uuid"
+    {:name ::weightset-selection-sidebar :view weightset/weightset-selection-sidebar}]
+   ["/weightset/:uuid/delete"
+    {:name      ::weightset-delete-sidebar
+     :view      weightset/weightset-delete-sidebar
+     :before-fx weightset/weightset-delete-before-fx}]
+   ["/weightset/:uuid/weight_create"
+    {:name      ::weight-create-sidebar
+     :view      weight/weight-create-sidebar
+     :before-fx weight/weight-create-before-fx}]
    
-   ["/input/:uuid/delete"             {:name      ::input-delete-sidebar
-                                       :view      input/input-delete-sidebar
-                                       :before-fx input/input-delete-before-fx}]
-   ["/loss/:uuid/delete"              {:name      ::loss-delete-sidebar
-                                       :view      loss/loss-delete-sidebar
-                                       :before-fx loss/loss-delete-before-fx}]])
+   ["/weight/:uuid"
+    {:name ::weight-sidebar :view weight/weight-sidebar}]
+   ["/weight_selection/:uuid"
+    {:name      ::weight-selection-sidebar
+     :view      weight/weight-selection-sidebar
+     :before-fx weight/weight-selection-before-fx}]
+   ["/weight/:uuid/delete"
+    {:name      ::weight-delete-sidebar
+     :view      weight/weight-delete-sidebar
+     :before-fx weight/weight-delete-before-fx}]
+   
+   ["/input/:uuid/delete"
+    {:name      ::input-delete-sidebar
+     :view      input/input-delete-sidebar
+     :before-fx input/input-delete-before-fx}]
+   ["/loss/:uuid/delete"
+    {:name      ::loss-delete-sidebar
+     :view      loss/loss-delete-sidebar
+     :before-fx loss/loss-delete-before-fx}]])
 
 (defn sidebar []
   (let [curr-sidebar-match @(subscribe [:curr-sidebar-match])]
