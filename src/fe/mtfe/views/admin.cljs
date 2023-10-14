33cf6a154baf3c1571672a6aac52268e35fb020b
Howon Lee <hlee.howon@gmail.com>
2023-10-14 08:55:32 -0700

    look, ma, some admin stuff

--- src/fe/mtfe/views/admin.cljs
+++ src/fe/mtfe/views/admin.cljs
@@ -12,11 +12,22 @@
                   (api/mt-user-password-login)
                   {}]]]])
 
-;; [:selection :password-logins (api/password-login) {}]
+(defn login-view [curr-password-login]
+  (let [message (if (seq curr-password-login)
+                  "Password"
+                  "None")]
+    [sc/table-member message
+     ;; link to deletion or creation or whatever
+     ]))
 
 (defn admin-page [m]
   (let [mt-users @(subscribe [:selection
-                              :mt-user-password-logins])]
+                              :mt-user-password-logins
+                              :mertonon.mt-users])
+        grouped-logins (group-by :mt-user-uuid @(subscribe [:selection
+                              :mt-user-password-logins
+                              :mertonon.password-logins]))
+        printo         (println grouped-logins)]
     [:div.fl.pa2
      [:h1 "Admin"]
      [:p "There are no non-admin accounts at this time. We will add normal user accounts when we do it."]
@@ -27,18 +38,18 @@
          [:thead
           [:tr
            [sc/table-head "UUID"]
-           [sc/table-head "Username"]
-           [sc/table-head "Email"]
+           [sc/table-head "Username - Email"]
            [sc/table-head "Created Date"]
            [sc/table-head "Updated Date"]
+           [sc/table-head "Authentication Type"]
            [sc/table-head ""]]]
          [:tbody
           (for [mt-user mt-users] ^{:key (:uuid mt-user)}
             [:tr
              [sc/table-member (str (:uuid mt-user))]
-             [sc/table-member (str (:username mt-user))]
-             [sc/table-member (str (:email mt-user))]
+             [sc/table-member (str (mt-user :username) " - " (mt-user :email))]
              [sc/table-member (str (:created-at mt-user))]
              [sc/table-member (str (:updated-at mt-user))]
+             [login-view (grouped-logins (mt-user :uuid))]
              [sc/table-member [util/sl (util/path ["mt_user" (str (:uuid mt-user)) "delete"])
                                [sc/trash-icon]]]])]]])]))
