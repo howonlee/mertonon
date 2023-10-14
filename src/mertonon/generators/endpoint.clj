f71eccbdef6ab67c0b07a8972d671b2468d2932d
Howon Lee <hlee.howon@gmail.com>
2023-10-14 10:31:51 -0700

    generation done for multi

--- src/mertonon/generators/endpoint.clj
+++ src/mertonon/generators/endpoint.clj
@@ -3,8 +3,15 @@
   (:require [clojure.string :as str]
             [clojure.test.check.generators :as gen]
             [mertonon.generators.queries :as queries-gen]
+            [mertonon.api.util :as api-util]
             [tick.core :as t]))
 
 (def join-endpoint-gen
   (gen/let [join-q queries-gen/generate-single-joined-query]
-    nil))
+    (let [{table            :table
+           table->model     :table->model} join-q
+          curr-model                       (table->model table)
+          endpoint                         (api-util/get-joined-models curr-model join-q)]
+      endpoint)))
+
+(comment (println (gen/generate join-endpoint-gen)))
