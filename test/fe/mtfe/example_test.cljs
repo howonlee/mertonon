ba842bc8d84c1681b55f85d922c78fa25a70954f
Howon Lee <hlee.howon@gmail.com>
2023-10-25 11:31:31 -0700

    it compiles

--- test/fe/mtfe/example_test.cljs
+++ test/fe/mtfe/example_test.cljs
@@ -1,5 +1,6 @@
 (ns mtfe.example-test
-  (:require [cljs.test :refer [deftest is]]))
+  (:require [cljs.test :refer [deftest is]]
+            [mertonon.generators.net :as net-gen]))
 
 (deftest a-test
   (is (= 1 2)))
