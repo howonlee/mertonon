3c70839ecdf8ffc607defa91303c1aa109791c4f
Howon Lee <hlee.howon@gmail.com>
2023-10-30 18:08:36 -0700

    thinking

--- src/fe/mtfe/generators/net_store.cljs
+++ src/fe/mtfe/generators/net_store.cljs
@@ -1,7 +1,7 @@
 (ns mtfe.generators.net-store
   "Sometimes, we generate the net itself, which can easily be done with mertonon.generators.net.
   Sometimes, we want to exercise everything up to and including API calls,
-  which therefore needs to have basically the contents of the BE dumped in them.
+  which therefore needs to have basically the contents of the net BE dumped in them.
   
   But it's a hard ask to dump everything for every test, so here's this in-memory thing here to dump into"
   (:require [ajax.core :refer [GET POST]]
