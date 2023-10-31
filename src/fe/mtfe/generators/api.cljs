1aa1b12ae573e491562a577d4235633af5ac6691
Howon Lee <hlee.howon@gmail.com>
2023-10-30 22:09:26 -0700

    thinking

--- src/fe/mtfe/generators/api.cljs
+++ src/fe/mtfe/generators/api.cljs
@@ -4,16 +4,28 @@
             [mtfe.api :as api]
             [mtfe.generators.net-store :as net-store]))
 
-(defn non-generator-api? [k]
-  (not (clojure.string/includes? (name k) "generator")))
+(defn valid-api? [k]
+  (and
+    (not= (name k) "host")
+    (not (clojure.string/includes? (name k) "generator"))))
 
 (def api-endpoint-fns
   (let [publics (ns-publics 'mtfe.api)
-        publics (select-keys publics (for [[k v] publics :when (non-generator-api? k)] k))
+        publics (select-keys publics (for [[k v] publics :when (valid-api? k)] k))
         publics (-> publics vals vec)]
     publics))
 
-(def api-endpoints
-  nil)
+(def gen-api-endpoints
+  (gen/let [endpoint-fn (gen/elements api-endpoint-fns)
+            curr-idx    (gen/choose 0 10)]
+    (let [curr-meta (meta endpoint-fn)
+          arity     (-> curr-meta :arglists first count)]
+      (condp = arity
+        0 (endpoint-fn)
+        1 (endpoint-fn (-> (@net-store/store (curr-meta :table)) cycle (nth curr-idx) :uuid))
+        (str "currently-bad arity")))))
 
-(println api-endpoints)
+(comment
+  (try
+    (cljs.pprint/pprint (gen/generate gen-api-endpoints))
+    (catch :default e (println e))))
